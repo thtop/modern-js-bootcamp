72e6e8baaf0a4041dcda7c2e9ad0aa0e61befdbf
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-11-27 06:54:10 +0700

    finished section 24

--- 24-Authentication/index.js
+++ 24-Authentication/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
-const userRepo = require('./repositories/users');
+const usersRepo = require('./repositories/users');
 
 const app = express();
 
@@ -12,7 +12,7 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
   res.send(`
     <div>
     Your id is ${req.session.userId}
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
   `);
 });
 
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
   const { email, password, passwordConfirmation } = req.body;
 
   const exitingUser = await userRepo.getOneBy({ email });
@@ -47,6 +47,46 @@ app.post('/', async (req, res) => {
   req.session.userId = user.id;
 
   res.send('Account created!!!');
+});
+
+app.get('/signout', (req, res) => {
+  req.session = null;
+  res.send('You are logout');
+});
+
+app.get('/signin', (req, res) => {
+  res.send(`
+  <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <button>Sign In</button>
+      </form>
+    </div>
+  `);
+});
+
+app.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await usersRepo.getOneBy({ email });
+
+  if (!user) {
+    return res.send('Email not found');
+  }
+
+  const validPassword = await usersRepo.comparePasswords(
+    user.password,
+    password
+  );
+
+  if (!validPassword) {
+    return res.send('Invalid password');
+  }
+
+  req.session.userId = user.id;
+
+  res.send('You are signed in!!!');
 });
 
 app.listen(3000, () => {
