2e0232a4e1d7277c8fdef4a7613ed8d9d01691ea
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-11-27 16:28:42 +0700

    finished item 352

---
+++ 25-Structuring-Projects/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const authRouter = require('./routes/admin/auth');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ['lkasld235']
+  })
+);
+
+app.use(authRouter);
+
+app.listen(3000, () => {
+  console.log('Listening');
+});
---
+++ 25-Structuring-Projects/views/admin/layout.js
@@ -0,0 +1,13 @@
+module.exports = ({ content }) => {
+  return `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>E-Commerce App</title>
+      </head>
+      <body>
+        ${content}
+      </body>
+    </html>
+  `;
+};
